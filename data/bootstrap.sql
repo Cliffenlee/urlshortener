6cfd13f452fc61bdcc29b063346e20a9e9bb891e
CliffenLee <cliffen.lee.2018@sis.smu.edu.sg>
2021-06-27 18:34:26 +0800

    edited db script to allow for longer urls

--- data/bootstrap.sql
+++ data/bootstrap.sql
@@ -9,7 +9,7 @@ USE urlshortener;
 DROP TABLE IF EXISTS url;
 CREATE TABLE IF NOT EXISTS url (
   	id int auto_increment NOT NULL,
-    long_url VARCHAR(200) NOT NULL,
+    long_url VARCHAR(1000) NOT NULL,
 	code VARCHAR(8) UNIQUE NOT NULL,
   	PRIMARY KEY (id)
 );
