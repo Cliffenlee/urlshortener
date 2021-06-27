8a9e2e5f8ab6d91f5bf31444668ae7fe88ea1f52
CliffenLee <cliffen.lee.2018@sis.smu.edu.sg>
2021-06-27 18:34:02 +0800

    added frontend modal to display shortened url

--- frontend/urlshortener/pages/index.js
+++ frontend/urlshortener/pages/index.js
@@ -1,23 +1,34 @@
-import {Box, Button, Flex, Heading, Input} from "@chakra-ui/react";
-import { useRef } from "react";
+import {Box, Button, Flex, Heading, Input, InputGroup, InputLeftAddon, InputRightAddon, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure} from "@chakra-ui/react";
+import { useRef, useState } from "react";
+import {LinkIcon} from "@chakra-ui/icons";
 import validator from "validator";
 import axios from "axios";
 
 export default function Home() {
 
   const url = useRef()
-  const URL = "http://localhost:8080"
+  const URL = "http://localhost:8080/"
+  const [result, setResult] = useState("")
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   async function getShortenedUrl () {
     if (validator.isURL(url.current.value)) {
       console.log("valid")
       const request = {long_url: url.current.value}
-      const response = await axios.post(URL+"/url", request)
+      const response = await axios.post(URL+"url", request)
 
       if (response.status == 200) {
-        const url = URL+response.data.long_url
+        console.log(response)
+        const url = URL+response.data.code
         console.log(url)
         //show success message
+        setResult(
+            <InputGroup size="md">
+              <InputLeftAddon children={<LinkIcon/>}/>
+              <Input _placeholder={{ color: 'black' }} fontWeight="500" color="black" placeholder={url} disabled={true}/>
+              <Button colorScheme="green" onClick={() => {navigator.clipboard.writeText(url)}}>Copy Link</Button>
+            </InputGroup>)
+        onOpen()
       } else {
         // error message
 
@@ -29,14 +40,30 @@ export default function Home() {
   }
 
   return (
-    <Flex flexDirection="column" width="100vw" height="100vh" alignItems="center" backgroundColor="gray.100">
-      <Box height={20} width="100%" textAlign="center" backgroundColor="blue.100">
+    <Flex position="relative" flexDirection="column" width="100vw" height="100vh" alignItems="center" justifyContent="center" backgroundColor="gray.100">
+      <Box position="absolute" top="0" left="0" height={20} width="100%" textAlign="center" backgroundColor="blue.100">
           <Heading color="black" mt={15} size="lg">URL Shortener</Heading>
       </Box>
-      <Flex flexDirection="column" width="50%" height="50%" alignItems="center" padding={8} borderRadius={20} mt="20vh" backgroundColor="white">
-          <Input ref={url} mt={20} width="70%" placeholder="Input your URL!" size="md"/>
-          <Button onClick={getShortenedUrl} width="20%" mt={10} colorScheme="blue">Shorten</Button>
+      <Flex position="relative" flexDirection="column" width="50%" height="30%" alignItems="center" justifyContent="space-between" padding={8} borderRadius={20} backgroundColor="white">
+          <Input ref={url} mt={10} width="70%" placeholder="Input your URL!" size="md"/>
+          <Button position="absolute" bottom={10} onClick={getShortenedUrl} width="20%"colorScheme="blue">Shorten</Button>
       </Flex>
+      <Modal size="xl" isCentered isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Your link has been generated!</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {result}
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Flex>
   )
 }
