72ecb3ab798ebc18fd45c93f883ce3c471e168d5
CliffenLee <cliffen.lee.2018@sis.smu.edu.sg>
2021-06-27 13:54:18 +0800

    created bootstrap file for data structure

---
+++ data/bootstrap.sql
@@ -0,0 +1,15 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+SET time_zone = "+08:00";
+
+drop schema if exists urlshortener;
+CREATE schema urlshortener DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE urlshortener;
+
+DROP TABLE IF EXISTS url;
+CREATE TABLE IF NOT EXISTS url (
+  	id int auto_increment NOT NULL,
+    long_url VARCHAR(200) NOT NULL,
+	code VARCHAR(8) UNIQUE NOT NULL,
+  	PRIMARY KEY (id)
+);
