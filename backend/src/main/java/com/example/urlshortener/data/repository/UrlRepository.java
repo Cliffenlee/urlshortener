4e9abc556e41b137df8dacda178256b98e8e9670
CliffenLee <cliffen.lee.2018@sis.smu.edu.sg>
2021-06-27 20:20:27 +0800

    created repository for URL objects

--- backend/src/main/java/com/example/urlshortener/data/repository/UrlRepository.java
+++ backend/src/main/java/com/example/urlshortener/data/repository/UrlRepository.java
@@ -1,7 +1,12 @@
 package com.example.urlshortener.data.repository;
 
 import com.example.urlshortener.data.model.Url;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UrlRepository extends CrudRepository<Url, Integer> {
+
+    Optional<Url> findByCode (String code);
 }
