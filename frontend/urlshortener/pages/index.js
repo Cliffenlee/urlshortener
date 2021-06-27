e35f9721bb9d14faa8933a43932a4047d2181b7a
CliffenLee <cliffen.lee.2018@sis.smu.edu.sg>
2021-06-27 20:35:02 +0800

    added toasts for error messages

--- frontend/urlshortener/pages/index.js
+++ frontend/urlshortener/pages/index.js
@@ -1,4 +1,4 @@
-import {Box, Button, Flex, Heading, Input, InputGroup, InputLeftAddon, InputRightAddon, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure} from "@chakra-ui/react";
+import {Box, Button, Flex, Heading, Input, InputGroup, InputLeftAddon, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Spinner, useDisclosure, useToast} from "@chakra-ui/react";
 import { useRef, useState } from "react";
 import {LinkIcon} from "@chakra-ui/icons";
 import validator from "validator";
@@ -9,34 +9,70 @@ export default function Home() {
   const url = useRef()
   const URL = "http://localhost:8080/"
   const [result, setResult] = useState("")
+  const [button, setButton] = useState(<Button  onClick={getShortenedUrl} width="20%"colorScheme="blue">Shorten</Button>)
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const toast = useToast()
 
   async function getShortenedUrl () {
-    if (validator.isURL(url.current.value)) {
-      console.log("valid")
-      const request = {long_url: url.current.value}
-      const response = await axios.post(URL+"url", request)
+    try {
+      setButton(
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.500"
+          size="xl"
+        />
+      )
+      if (validator.isURL(url.current.value)) {
+        console.log("valid")
+        const request = {long_url: url.current.value}
+        const response = await axios.post(URL+"url", request)
 
-      if (response.status == 200) {
-        console.log(response)
-        const url = URL+response.data.code
-        console.log(url)
-        //show success message
-        setResult(
-            <InputGroup size="md">
-              <InputLeftAddon children={<LinkIcon/>}/>
-              <Input _placeholder={{ color: 'black' }} fontWeight="500" color="black" placeholder={url} borderTopEndRadius={0} borderBottomEndRadius={0} disabled={true}/>
-              <Button borderTopStartRadius={0} borderBottomStartRadius={0} colorScheme="green" onClick={() => {navigator.clipboard.writeText(url)}}>Copy Link</Button>
-            </InputGroup>)
-        onOpen()
+        if (response.status == 200) {
+          console.log(response)
+          const resultUrl = URL+response.data.code
+          console.log(url)
+          setResult(
+              <InputGroup size="md">
+                <InputLeftAddon children={<LinkIcon/>}/>
+                <Input _placeholder={{ color: 'black' }} fontWeight="500" color="black" placeholder={resultUrl} borderTopEndRadius={0} borderBottomEndRadius={0} disabled={true}/>
+                <Button borderTopStartRadius={0} borderBottomStartRadius={0} colorScheme="green" onClick={() => {navigator.clipboard.writeText(resultUrl)}}>Copy Link</Button>
+              </InputGroup>)
+          onOpen()
+        } else {
+          toast({
+            title: "Failed",
+            description: "Failed to shorten URL.",
+            status: "error",
+            duration: 5000,
+            isClosable: true,
+          })
+        }
       } else {
-        // error message
-
+        toast({
+          title: "Invalid",
+          description: "Please enter a valid URL!",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        })
       }
-    } else {
-      console.log("invalid")
-      //error message
+    } catch (error) {
+      console.log(error)
+      toast({
+        title: "Failed",
+        description: "Failed to shorten URL.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      })
+    } finally {
+      setButton(
+        <Button onClick={getShortenedUrl} width="20%"colorScheme="blue">Shorten</Button>
+      )
     }
+
   }
 
   return (
@@ -44,9 +80,9 @@ export default function Home() {
       <Box position="absolute" top="0" left="0" height={20} width="100%" textAlign="center" backgroundColor="blue.100">
           <Heading color="black" mt={15} size="lg">URL Shortener</Heading>
       </Box>
-      <Flex position="relative" flexDirection="column" width="50%" height="30%" alignItems="center" justifyContent="space-between" padding={8} borderRadius={20} backgroundColor="white">
+      <Flex flexDirection="column" width="50%" height="25%" alignItems="center" justifyContent="space-between" padding={8} borderRadius={20} backgroundColor="white">
           <Input ref={url} mt={10} width="70%" placeholder="Input your URL!" size="md"/>
-          <Button position="absolute" bottom={10} onClick={getShortenedUrl} width="20%"colorScheme="blue">Shorten</Button>
+          {button}
       </Flex>
       <Modal size="xl" isCentered isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
